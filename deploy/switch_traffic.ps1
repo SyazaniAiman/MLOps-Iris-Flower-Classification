8a414ec0d5d0c585c24cf5cd78062f02106c0950
Muhammad Syazaniaiman Bin Mohd Shahimi <117069194+SyazaniAiman@users.noreply.github.com>
2026-02-05 23:45:16 +0800

    Add parameter for traffic color in switch script

--- deploy/switch_traffic.ps1
+++ deploy/switch_traffic.ps1
@@ -1,1 +1,20 @@
+param(
+  [Parameter(Mandatory=$true)]
+  [ValidateSet("blue","green")]
+  [string]$Color
+)
+
+$repoRoot = Resolve-Path (Join-Path $PSScriptRoot "..")
+$confDir  = Join-Path $repoRoot "nginx\conf.d"
+
+$template = Join-Path $confDir "$Color.conf.template"
+$default  = Join-Path $confDir "default.conf"
+
+Copy-Item -Force $template $default
+
+docker exec iris-nginx nginx -t
+if ($LASTEXITCODE -ne 0) { throw "Nginx config test failed" }
+
+docker exec iris-nginx nginx -s reload
+Write-Output "Switched traffic to $Color"
 
